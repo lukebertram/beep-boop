6f9b4dc9ef9d9ca56d18b3ef19ceaf80d35c2f33
Luke Bertram <lukebertram@gmail.com>
2017-10-27 11:51:48 -0700

    pass second spec: replaces numbers containing 1 with boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
-//backend
+//back-end business
 var halProcess = function(inputNum){
   var outputArray = [];
+  debugger
   for (var i = 1; i <= inputNum; i++) {
-    outputArray.push(i);
+    var newElement;
+    // if this integer contains a one...
+    if (containsOne(i)){
+      // ... set new element to "Boop!"
+      newElement = "Boop!";
+    } else {
+      // otherwise set new element to the index integer
+      newElement = i;
+    }
+    outputArray.push(newElement);
   }
   return outputArray.join(", ");
 };
 
-//front-end
+var containsOne = function(integer){
+  var testStr = integer.toString();
+  for (var i = 0; i < testStr.length; i++) {
+    if (testStr.charAt(i) === "1"){
+      return true
+    }
+  }
+  return false;
+}
+
+//front-end foolery
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
