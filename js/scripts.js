20244c8b3f4c3576889ac1e7ef71891f24254ae2
Luke Bertram <lukebertram@gmail.com>
2017-10-27 10:46:17 -0700

    pass first spec: outputs sequence of numbers 1 to input

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 //backend
-
+var halProcess = function(inputNum){
+  var outputArray = [];
+  for (var i = 1; i <= inputNum; i++) {
+    outputArray.push(i);
+  }
+  return outputArray.join(", ");
+};
 
 //front-end
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
     var input = parseInt($("#numberEntry").val());
-    var output = input;
+    var output = halProcess(input);
     $("div#result").text(output)
   });
 });
